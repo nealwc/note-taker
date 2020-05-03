78a73fa4d6dfaf1166b85e62d3ae62c7f71c6b84
Carter Neal <55357370+nealwc@users.noreply.github.com>
2020-05-03 16:18:08 -0400

    added comments

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,10 +3,12 @@ const fs = require("fs");
 const uuid = require("uuid").v4;
 
 module.exports = function (app) {
+    // function to retrieve the note data from the db.json file
     app.get("/api/notes", function (req, res) {
         return res.json(db);
     });
 
+    // function to post new note data to the db.json file
     app.post("/api/notes", function (req, res) {
         let newNote =
         {
@@ -15,11 +17,13 @@ module.exports = function (app) {
             text: req.body.text
         };
 
+        // retrieves the data from the db.json, parses the data and adds the new note
         fs.readFile("./db/db.json", (err, data) => {
             if (err) throw err;
             let notesData = JSON.parse(data);
             notesData.push(newNote);
 
+            // turns the new notes data into strings for JSON and writes it to the db.json file
             fs.writeFile("./db/db.json", JSON.stringify(notesData), function (err) {
                 if (err) throw err;
                 return res.json(db);
@@ -27,6 +31,7 @@ module.exports = function (app) {
         });
     });
 
+    // funciton to delete a note from the db.json file
     app.delete("api/notes:id", function (req, res) {
         let chosenId = req.params.id;
         console.log(chosenId);
